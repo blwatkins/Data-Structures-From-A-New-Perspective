3938c7148aa0874f5a353bdd2e095ed2a78746ab
Brittni Watkins <blwatkins@smu.edu>
2017-04-20 17:03:17 -0500

    Add multiple file viewing functionality.

--- 3_Sorting/Homework/Homework1/Solution/src/Solution.java
+++ 3_Sorting/Homework/Homework1/Solution/src/Solution.java
@@ -1,42 +1,77 @@
 // Brittni Watkins
 // Unit 3 - Sorting
 // Homework 1 Solution
+// Text Sources
+    // Wordsworth -> http://www.gutenberg.org/cache/epub/8905/pg8905.txt
+    // Poe -> http://www.gutenberg.org/cache/epub/10031/pg10031.txt
+    // Poe -> http://www.gutenberg.org/cache/epub/1063/pg1063.txt
+    // Dickinson -> http://www.gutenberg.org/cache/epub/2679/pg2679.txt
+    // Teasdale -> http://www.gutenberg.org/cache/epub/442/pg442.txt
 
 import processing.core.*;
 import processing.data.*;
-import processing.event.*;
-import processing.opengl.*;
 
-import java.util.HashMap;
 import java.util.ArrayList;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
-
 import java.util.Arrays;
 
 public class Solution extends PApplet {
-    private String[] rawText;
+    private String[] filenames = {"dickinson_1.txt", "dickinson_2.txt", "dickinson_3.txt", "poe_amontillado.txt",
+            "poe_bells.txt", "poe_raven.txt", "teasdale_barter.txt", "teasdale_fountain.txt",
+            "teasdale_twilight.txt", "wordsworth_animal.txt", "wordsworth_complaint.txt", "wordsworth_tables.txt",
+            "dickinson.txt", "poe.txt", "teasdale.txt", "wordsworth.txt"};
 
+    private String[] rawText;
     private int maxStringLength;
     private int minStringLength;
 
     private int rows;
     private int cols;
-
     private float maxW;
     private float minW;
-
     private float maxH;
     private float minH;
 
+    private int currentFileIndex;
+
     public void setup() {
         colorMode(HSB, 255);
-        String filename = "test.txt";
-        rawText = getText(filename);
+        currentFileIndex = 0;
+        _init();
+    }
+
+    public void draw() {
+        background(200);
+        displayWords(rawText);
+    }
+
+    public void settings() {
+        size(displayWidth, displayHeight);
+    }
+
+    public void keyPressed() {
+
+        if (keyCode == RIGHT) {
+            currentFileIndex++;
+
+            if (currentFileIndex >= filenames.length) {
+                currentFileIndex = 0;
+            }
+
+            _init();
+        } else if (keyCode == LEFT) {
+            currentFileIndex--;
+
+            if (currentFileIndex < 0) {
+                currentFileIndex = filenames.length - 1;
+            }
+
+            _init();
+        }
+
+    }
+
+    private void _init() {
+        rawText = getText(filenames[currentFileIndex]);
 
         int[] factors = findFactors(rawText.length);
         rows = min(factors[0], factors[1]);
@@ -53,30 +88,6 @@ public class Solution extends PApplet {
 
         minH = maxH / 10;
         if (minH < 1) minH = 1;
-    }
-
-    public void draw() {
-
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < cols; j++) {
-                String word = rawText[(i * cols) + j].toLowerCase();
-                float ellipseW = map(word.length(), minStringLength, maxStringLength, minW, maxW);
-                float ellipseH = map(word.length(), minStringLength, maxStringLength, minH, maxH);
-                float ellipseX = (j * maxW) + (maxW / 2);
-                float ellipseY = (i * maxH) + (maxH / 2);
-                int hue = (int)map((int)word.charAt(0), (int)'a', (int)'z', 0, 255);
-
-                fill(hue, 200, 200);
-                ellipse(ellipseX, ellipseY, ellipseW, ellipseH);
-            }
-
-        }
-
-    }
-
-    public void settings() {
-        size(displayWidth, displayHeight);
     }
 
     private String[] getText(String file) {
@@ -131,6 +142,27 @@ public class Solution extends PApplet {
         }
 
         return minL;
+    }
+
+    private void displayWords(String[] text) {
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+                String word = text[(i * cols) + j].toLowerCase();
+                float ellipseW = map(word.length(), minStringLength, maxStringLength, minW, maxW);
+                float ellipseH = map(word.length(), minStringLength, maxStringLength, minH, maxH);
+                float ellipseX = (j * maxW) + (maxW / 2);
+                float ellipseY = (i * maxH) + (maxH / 2);
+                int hue = (int)map((int)word.charAt(0), (int)'a', (int)'z', 0, 255);
+
+                fill(hue, 200, 200);
+                noStroke();
+                ellipse(ellipseX, ellipseY, ellipseW, ellipseH);
+            }
+
+        }
+
     }
 
     static public void main(String[] passedArgs) {
