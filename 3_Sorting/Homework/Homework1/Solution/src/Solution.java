4a9c52adb6c1a64c01c5ed48f19d4afa491eb4ea
Brittni Watkins <blwatkins@smu.edu>
2017-04-20 09:14:40 -0500

    Display text as circles. Determine max and min length and sizes. Add additional testing file.

--- 3_Sorting/Homework/Homework1/Solution/src/Solution.java
+++ 3_Sorting/Homework/Homework1/Solution/src/Solution.java
@@ -16,40 +16,121 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
+import java.util.Arrays;
+
 public class Solution extends PApplet {
-    String filename;
-    String[] rawText;
+    private String[] rawText;
+
+    private int maxStringLength;
+    private int minStringLength;
+
+    private int rows;
+    private int cols;
+
+    private float maxW;
+    private float minW;
+
+    private float maxH;
+    private float minH;
 
     public void setup() {
-        filename = "test.txt";
+        colorMode(HSB, 255);
+        String filename = "test.txt";
         rawText = getText(filename);
+
+        int[] factors = findFactors(rawText.length);
+        rows = min(factors[0], factors[1]);
+        cols = max(factors[0], factors[1]);
+
+        maxStringLength = getMaxLength(rawText);
+        minStringLength = getMinLength(rawText);
+
+        maxW = (float)width / cols;
+        maxH = height / (rows + 0.5f);
+
+        minW = maxW / 10;
+        if (minW < 1) minW = 1;
+
+        minH = maxH / 10;
+        if (minH < 1) minH = 1;
     }
 
     public void draw() {
 
-    }
-    public void settings() {
-        //size(displayWidth, displayHeight);
-        size(500, 500);
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+                String word = rawText[(i * cols) + j].toLowerCase();
+                float ellipseW = map(word.length(), minStringLength, maxStringLength, minW, maxW);
+                float ellipseH = map(word.length(), minStringLength, maxStringLength, minH, maxH);
+                float ellipseX = (j * maxW) + (maxW / 2);
+                float ellipseY = (i * maxH) + (maxH / 2);
+                int hue = (int)map((int)word.charAt(0), (int)'a', (int)'z', 0, 255);
+
+                fill(hue, 200, 200);
+                ellipse(ellipseX, ellipseY, ellipseW, ellipseH);
+            }
+
+        }
+
     }
 
-    String[] getText(String file) {
+    public void settings() {
+        size(displayWidth, displayHeight);
+    }
+
+    private String[] getText(String file) {
         String[] lines = loadStrings(file);
         ArrayList<String> text = new ArrayList<>();
 
         for (String line: lines) {
             String[] lineText = splitTokens(line);
-
-            for (String token: lineText) {
-                text.add(token);
-            }
-
+            text.addAll(Arrays.asList(lineText));
         }
 
         String[] textArray = new String[text.size()];
         textArray = text.toArray(textArray);
 
         return textArray;
+    }
+
+    private int[] findFactors(int num) {
+        IntList factors = new IntList();
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                factors.append(i);
+            }
+        }
+
+        int factor1_index = factors.size() / 2;
+        int factor2_index = (factors.size() / 2) - 1;
+
+        int[] maxFactors = new int[2];
+        maxFactors[0] = factors.get(factor1_index);
+        maxFactors[1] = factors.get(factor2_index);
+
+        return maxFactors;
+    }
+
+    private int getMaxLength(String[] text) {
+        int maxL = 0;
+
+        for (String token: text) {
+            maxL  = max(maxL, token.length());
+        }
+
+        return maxL;
+    }
+
+    private int getMinLength(String[] text) {
+        int minL = text[0].length();
+
+        for (String token: text) {
+            minL = min(minL, token.length());
+        }
+
+        return minL;
     }
 
     static public void main(String[] passedArgs) {
