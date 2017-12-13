3f288b75bcd58210237df3daa971b5e997cf1b24
Brittni Watkins <blwatkins@smu.edu>
2017-12-12 18:13:39 -0600

    Add function to create element object from JSON file.

--- 4_LinkedLists/ProjectSolution/src/DataVisualization.java
+++ 4_LinkedLists/ProjectSolution/src/DataVisualization.java
@@ -5,11 +5,22 @@
     // periodicTable.json --> https://github.com/Bowserinator/Periodic-Table-JSON/blob/master/PeriodicTableJSON.json
 
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 public class DataVisualization extends PApplet {
+    private String filename = "periodicTable.json";
 
     public void setup() {
+        JSONObject elementsObject = loadJSONObject(filename);
+        JSONArray elements = elementsObject.getJSONArray("elements");
 
+        try {
+            Element e = createElement(elements.getJSONObject(0));
+            println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     public void draw() {
@@ -19,6 +30,16 @@ public class DataVisualization extends PApplet {
     public void settings() {
         //size(displayWidth, displayHeight-45);
         size(600, 600);
+    }
+
+    private Element createElement(JSONObject elementObject) throws Exception {
+        Element e = new Element(this);
+        e.setName(elementObject.getString("name"));
+        e.setSymbol(elementObject.getString("symbol"));
+        e.setAtomicNumber(elementObject.getInt("number"));
+        e.setAtomicMass(elementObject.getFloat("atomic_mass"));
+        e.setDensity(elementObject.getFloat("density"));
+        return e;
     }
 
     public static void main(String[] passedArgs) {
--- 4_LinkedLists/ProjectSolution/src/Element.java
+++ 4_LinkedLists/ProjectSolution/src/Element.java
@@ -8,15 +8,43 @@ import processing.core.PApplet;
 public class Element {
     PApplet p;
 
-    String name;
-    String symbol;
-    int atomicNumber;
-    float atomicMass;
-    float density;
-
+    private String name;
+    private  String symbol;
+    private int atomicNumber;
+    private float atomicMass;
+    private float density;
 
     public Element(PApplet p) {
         this.p = p;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public void setAtomicNumber(int atomicNumber) {
+     this.atomicNumber = atomicNumber;
+    }
+
+    public void setAtomicMass(float atomicMass) {
+        this.atomicMass = atomicMass;
+    }
+
+    public void setDensity(float density) {
+        this.density = density;
+    }
+
+    public String toString() {
+        String out = "Element: " + '\n';
+        out += "    name = " + name + '\n';
+        out += "    symbol = " + symbol + '\n';
+        out += "    atomicNumber = " + atomicNumber + '\n';
+        out += "    atomicMass = " + atomicMass + '\n';
+        return out;
     }
 
 
