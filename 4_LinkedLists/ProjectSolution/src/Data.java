137c0089122e2273c76b194dcbf368fcd373913e
Brittni Watkins <blwatkins@smu.edu>
2017-12-12 19:09:55 -0600

    Add new class for static data processing methods.

---
+++ 4_LinkedLists/ProjectSolution/src/Data.java
@@ -0,0 +1,56 @@
+// Brittni Watkins
+// Unit 4: Linked Lists
+// Project - Data Visualization
+// Data Class
+
+import processing.core.PApplet;
+
+public class Data {
+
+    private static PApplet p;
+
+    public static void init(PApplet context) {
+        p = context;
+    }
+
+    public static int getMaxInt(int[] array) {
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            max = p.max(max, array[i]);
+        }
+
+        return max;
+    }
+
+    public static int getMinInt(int[] array) {
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            min = p.min(min, array[i]);
+        }
+
+        return min;
+    }
+
+    public static float getMaxFloat(float[] array) {
+        float max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            max = p.max(max, array[i]);
+        }
+
+        return max;
+    }
+
+    public static float getMinFloat(float[] array) {
+        float min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            min = p.min(min, array[i]);
+        }
+
+        return min;
+    }
+
+}
